1417cff1dbafc4574e2df43b279a2d8c1e065f38
Wibaek Park <devmuromi@gmail.com>
2025-07-23 17:07:20 +0900

    feat: User, PartnerUniversity, Application 모델 추가

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,28 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DB = os.getenv("MYSQL_DB")
 
-Base = declarative_base() import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
 from datetime import UTC, datetime
 from app.core.database import Base
 
@@ -7,33 +6,39 @@ from app.core.database import Base
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, index=True)
-    uuid = Column(String(36), unique=True)
-    nickname = Column(String(255))
-    status = Column(Integer, default=0)  # 0: 미인증, 1: 인증, 2: 성적대기중
-    grade = Column(Float)
-    lang = Column(String(255))
-    modify_count = Column(Integer, default=3)
-    created_at = Column(DateTime, default=datetime.now(UTC))
-    updated_at = Column(DateTime, default=datetime.now(UTC))
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    uuid = Column(String(36), unique=True, nullable=False)
+    nickname = Column(String(255), nullable=False)
+    status = Column(
+        Integer, default=0, nullable=False
+    )  # 0: 미인증, 1: 인증, 2: 성적대기중
+    grade = Column(Float, nullable=False)
+    lang = Column(String(255), nullable=False)
+    modify_count = Column(Integer, default=3, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=False
+    )
 
 
 class PartnerUniversity(Base):
     __tablename__ = "partner_university"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255))
-    country = Column(String(255))
-    slot = Column(Integer)
-    duration = Column(String)  # "1개학기" or "2개학기"
-    created_at = Column(DateTime, default=datetime.now(UTC))
-    updated_at = Column(DateTime, default=datetime.now(UTC))
+    name = Column(String(255), nullable=False)
+    country = Column(String(255), nullable=False)
+    slot = Column(Integer, nullable=False)
+    duration = Column(String(255), nullable=False)  # "1개학기" or "2개학기"
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
 
 
 class Application(Base):
     __tablename__ = "application"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("user.id"))
-    partner_university_id = Column(Integer, ForeignKey("partner_university.id"))
-    choice = Column(Integer)
-    created_at = Column(DateTime, default=datetime.now(UTC))
-    updated_at = Column(DateTime, default=datetime.now(UTC))
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    partner_university_id = Column(
+        Integer, ForeignKey("partner_university.id"), nullable=False
+    )
+    choice = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
